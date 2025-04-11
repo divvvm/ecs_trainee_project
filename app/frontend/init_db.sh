d501ec3bce9aee6f2fc941bc2ef25e63a4ba7e75
Denys Misiailo <dmisi@softserveinc.com>
2025-04-11 14:43:31 +0300

    Add debug to frontend

--- app/frontend/init_db.sh
+++ app/frontend/init_db.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "DB_HOST: $DB_HOST"
+echo "DB_PORT: $DB_PORT"
+echo "DB_USER: $DB_USER"
+echo "DB_PASSWORD: $DB_PASSWORD"
+echo "DB_NAME: $DB_NAME"
+
 until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
   echo "PostgreSQL is unavailable - sleeping"
   sleep 1
@@ -9,6 +15,7 @@ done
 echo "PostgreSQL is up - executing commands"
 
 PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "CREATE EXTENSION IF NOT EXISTS vector;"
+
 PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "CREATE TABLE IF NOT EXISTS messages (
   id SERIAL PRIMARY KEY,
   message TEXT NOT NULL,
