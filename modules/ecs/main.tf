60e67e46aa3e1afb6f11b0fd7eb17dd659590bee
Denys Misiailo <dmisi@softserveinc.com>
2025-04-27 18:05:49 +0300

    Change ECS module

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -74,7 +74,7 @@ resource "aws_ecs_service" "main" {
   name            = "${var.cluster_name}-${each.value.name}-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.main[each.key].arn
-  desired_count   = 2
+  desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
